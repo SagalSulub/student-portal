ed58605bfd5f010f5a62ba55e9ffb9599a545f84
Anne-Marie <dorschtannemarie@gmail.com>
2026-06-24 15:17:51 -0600

    Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <nav style={{ padding: "10px", borderBottom: "1px solid #ddd" }}>
-      <h2>New Generation High School</h2>
+      <h2>New Generation High School Portal</h2>
     </nav>
   )
 }
