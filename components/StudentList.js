712210f5973e4a674b497f1b6184e8b25502ab0e
ziyamahesaniya <ziyaamirali.mahesaniya@edu.sait.ca>
2026-06-25 15:24:01 -0600

    Add comment for displaying student list
    Added comment to clarify the purpose of the code.

--- components/StudentList.js
+++ components/StudentList.js
@@ -2,7 +2,7 @@ export default function StudentList({ students }) {
   return (
     <div>
       <h3>Students</h3>
-
+      // displying student list
       {students.map((s) => (
         <div key={s.id}>
           <p>{s.firstName} {s.lastName}</p>
