b85caf280f0e0fdd16a7cda369ce70a9dad29dcc
ziyamahesaniya <ziyaamirali.mahesaniya@edu.sait.ca>
2026-06-25 15:22:29 -0600

    Add comment to Navbar component
    Added a comment to the Navbar component.

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,6 @@
 export default function Navbar() {
   return (
+    // Navbar
     <nav style={{ padding: "10px", borderBottom: "1px solid #ddd" }}>
       <h2>New Generation High School Portal</h2>
     </nav>
