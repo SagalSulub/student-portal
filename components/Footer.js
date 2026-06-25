a9784616b69361d596e456b517b130013198a527
ziyamahesaniya <ziyaamirali.mahesaniya@edu.sait.ca>
2026-06-25 15:23:31 -0600

    Add comment to Footer component
    Added a comment to indicate the footer section.

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,6 @@
 export default function Footer() {
   return (
+    // Footer
     <footer style={{ marginTop: "30px", padding: "10px", borderTop: "1px solid #ddd" }}>
       <p>New Generation High School Student Portal</p>
     </footer>
